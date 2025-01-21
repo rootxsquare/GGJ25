2e546a0f3472407e47b8dccd98a5e441f4271a87
akshatsinha1 <akshat.sinha@techeducators.co.uk>
2025-01-21 16:54:46 +0000

    Bubble Blowing Mechanic and shader

---
+++ BubbleGGJ/Assets/Scripts/BubbleScript.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class BubbleScript : MonoBehaviour
+{
+     float t = 1;
+    [SerializeField] float bubbleLife = 5;
+    public GameObject bubblePopParticle;
+    bool isSimulating = false;
+    [SerializeField] float maxSize;
+    [SerializeField] float growSpeed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isSimulating) t -= Time.deltaTime / bubbleLife;
+
+
+        if (Input.GetKey(KeyCode.B) && !isSimulating)
+        {
+            transform.localScale += Vector3.one * Time.deltaTime * growSpeed;
+            if (transform.localScale.x > maxSize) bubblePop();
+        }
+        else if (Input.GetKeyUp(KeyCode.B))
+        {
+            if (transform.localScale.x < 0.25f) bubblePop();
+            bubbleLife = transform.localScale.x * 10;
+
+            bubbleSimulate();
+        }
+
+        transform.GetChild(0).GetComponent<SkinnedMeshRenderer>().material.SetFloat("_FresnelPower", t);
+
+        if(t<= 0.1f)
+        {
+            bubblePop();
+        }
+    }
+
+    void bubblePop()
+    {
+        GameObject popParticle = Instantiate(bubblePopParticle, transform.GetChild(1).position, Quaternion.identity);
+        popParticle.transform.localScale = transform.localScale;
+        Destroy(popParticle, 2);
+        Destroy(gameObject);
+
+    }
+
+    public void bubbleSimulate()
+    {
+        isSimulating = true;
+        transform.GetChild(1).gameObject.SetActive(true);
+        foreach(Rigidbody rb in GetComponentsInChildren<Rigidbody>())
+        {
+            rb.AddForce(Vector3.up *3);
+        }
+    }
+}
---
+++ BubbleGGJ/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public GameObject bubblePrefab;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.B)) Instantiate(bubblePrefab, transform.position, transform.rotation);
+    }
+}
